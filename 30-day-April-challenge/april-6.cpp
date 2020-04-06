cba03b5577a935133191fe56313e78a6c7530435
Mariia Kornieva <mariiakornieva@gmail.com>
2020-04-06 21:48:33 +0300

    Improve solution in terms of space comlexity

--- 30-day-April-challenge/april-6.cpp
+++ 30-day-April-challenge/april-6.cpp
@@ -29,3 +29,36 @@ public:
         return anagrams;
     }
 };
+
+
+// Improved Space complexity solution
+class Solution {
+private:
+    using vvs = std::vector<std::vector<std::string>>;
+    using vs = std::vector<std::string>;
+    
+    std::string getOccur(std::string const& str) {
+        std::string result(26, '0');
+        for (auto i : str) {
+            ++result[i-'a'];
+        }
+        return result;
+    }
+public:
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+        vvs anagrams;
+
+        std::map<std::string, int> hashMap;
+        int count = 0;
+        for (auto const& str : strs) {
+            auto occ = getOccur(str);
+            if (hashMap.find(occ) != std::end(hashMap)) {
+                anagrams[hashMap[occ]].push_back(str);
+            } else {
+                anagrams.push_back(vs{str});
+                hashMap[occ] = count++;
+            }
+        }
+        return anagrams;
+    }
+};
