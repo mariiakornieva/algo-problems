01e87554513620c6175a4d194cb8b997dd94c580
Mariia Kornieva <mariiakornieva@gmail.com>
2020-04-12 16:29:28 +0300

    Solve April-12 challenge using std::priority_queue

--- 30-day-April-challenge/april-12.cpp
+++ 30-day-April-challenge/april-12.cpp
@@ -2,7 +2,7 @@
 *   Last Stone Weight
 */
 
-class Solution {
+class SolutionWithMultiSet {
 private:
     int getMax(std::multiset<int>& stones) {
         int max = *std::prev(stones.cend());
@@ -23,5 +23,29 @@ public:
         }
 
         return stonesSet.empty() ? 0 : *stonesSet.begin();
+    }
+};
+
+class SolutionWithPQueue {
+private:
+    int getMax(std::priority_queue<int>& stones) {
+        int max = stones.top();
+        stones.pop();  
+        return max;
+    }
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        std::priority_queue<int> stonesQueue(stones.begin(), stones.end());
+        
+        while (stonesQueue.size() > 1) {
+            int max = getMax(stonesQueue);
+            int prev = getMax(stonesQueue);
+
+            if (max != prev) {
+                stonesQueue.push(max-prev);
+            }
+        }
+
+        return stonesQueue.empty() ? 0 : stonesQueue.top();
     }
 };
