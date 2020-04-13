6d31e26e65b9b9d31e2c38af990de87066714de0
Mariia Kornieva <mariiakornieva@gmail.com>
2020-04-13 22:54:06 +0300

    Create april-13.cpp

---
+++ 30-day-April-challenge/april-13.cpp
@@ -0,0 +1,6 @@
+// Contiguous Array
+// Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+// Input: [0,1]
+// Output: 2
+// Input: [0,1,0,0,0,1,1,0]
+// Output: 6
