e00d3d0a6d1e02c67b9f194e7c63ac844c49c45b
Mariia Kornieva <mariiakornieva@gmail.com>
2020-04-14 09:19:12 +0300

    Solve April-13 problem Contiguous Array

--- 30-day-April-challenge/april-13.cpp
+++ 30-day-April-challenge/april-13.cpp
@@ -4,3 +4,25 @@
 // Output: 2
 // Input: [0,1,0,0,0,1,1,0]
 // Output: 6
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int count = 0;
+        int len = 0;
+        // (count, index)
+        std::map<int, int> counts = {{0, -1}};
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i]) ++count;
+            else --count;
+            
+            if (counts.find(count) != std::end(counts)) {
+                len = std::max(len, i-counts[count]);
+            } else {
+                counts[count] = i;
+            }
+        }
+        
+        return len;
+    }
+};
