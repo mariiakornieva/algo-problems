372abf6dda98583c7642e1eaa8fd165ccfd2a4fe
Mariia Kornieva <mariiakornieva@gmail.com>
2020-04-16 09:59:49 +0300

    Update solution with C++17 algorithms

--- 30-day-April-challenge/april-15-product-array-except-self.cpp
+++ 30-day-April-challenge/april-15-product-array-except-self.cpp
@@ -2,10 +2,14 @@
 // Given an array nums of n integers where n > 1, 
 // return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
 
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
 // Pre C++17 solution
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
+    std::vector<int> productExceptSelf(std::vector<int> const& nums) {
         std::vector<int> products;
         std::vector<int> pref{1};
         std::vector<int> suf{1};
@@ -20,6 +24,28 @@ public:
             products.push_back(pref[i] * suf[n-1-i]);
         }
         
+        return products;
+    }
+};
+
+// Solution using C++ std::exclusive_scan
+class Solution17 {
+public:
+    std::vector<int> productExceptSelf(std::vector<int> const& nums) {
+        std::vector<int> pref(nums.size());
+        std::vector<int> suf(nums.size());
+        std::exclusive_scan(nums.begin(), nums.end(),
+                            pref.begin(), 1,
+                            std::multiplies<int>());
+        std::exclusive_scan(nums.rbegin(), nums.rend(),
+                            suf.rbegin(), 1,
+                            std::multiplies<int>());
+ 
+        std::vector<int> products(nums.size());
+        std::transform(pref.begin(), pref.end(),
+                       suf.begin(), products.begin(),
+                       std::multiplies<int>());
+    
         return products;
     }
 };
