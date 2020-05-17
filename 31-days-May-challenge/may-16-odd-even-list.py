220a708626cbb19a12754fe0dffc1e437f74ea21
Mariia Kornieva <mariiakornieva@gmail.com>
2020-05-17 09:55:07 +0300

    Solve May-16 challenge

---
+++ 31-days-May-challenge/may-16-odd-even-list.py
@@ -0,0 +1,29 @@
+'''
+  Odd Even Linked List
+Given a singly linked list, group all odd nodes together followed by the even nodes.
+Please note here we are talking about the node number and not the value in the nodes.
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        
+        odd = head
+        even = head.next
+        odd_head = head
+        even_head = head.next
+        
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        odd.next = even_head
+        return head
