9125c8508bf58f8f5d461ba745d2ccd5291d96cf
Mariia Kornieva <mariiakornieva@gmail.com>
2020-06-15 13:00:22 +0300

    Solve June-15th problem

---
+++ June-challenge/june-15-search-bst.py
@@ -0,0 +1,23 @@
+'''
+  Search in a Binary Search Tree
+Given the root node of a binary search tree (BST) and a value.
+You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node.
+If such node doesn't exist, you should return NULL.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return root
+        if val == root.val:
+            return root
+        elif val < root.val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
